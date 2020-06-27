b3a25ab828d08704b0a5358992a7727f410eb4bd
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-06-27 14:21:11 +0700

    Numbers

---
+++ src/main/kotlin/#013 Numbers.kt
@@ -0,0 +1,32 @@
+fun main(){
+    //Number Initilization
+    /*// Int (32 Bit)
+    val intNumbers = 100
+    // Long (64 Bit)
+    val longNumber: Long = 100
+    val longNumbers = 100L
+    // Short (16 Bit)
+    val shortNumber: Short = 10
+    // Byte (8 Bit)
+    val byteNumber = 0b11010010
+    // Double (64 Bit)
+    val doubleNumbers = 1.3*/
+
+    //Fungsi Max Min Values
+    /*val maxInt = Int.MAX_VALUE  //Fungsi untuk mengatahui batas nilai maksimal tipe data
+    val minInt = Int.MIN_VALUE  //Fungsi untuk mengatahui batas nilai manimal tipe data
+    println(maxInt)
+    println(minInt)*/
+
+    //Number Operation
+    /*val nilai1 = 10
+    val nilai2 = 20
+    println(nilai1 + nilai2)
+    println(nilai1 / nilai2)    //Karena int maka hasilnya di bulatkan
+    println((2 + 10) / 4)
+    println(3 * 4 + 5)*/
+
+    val nilai1 = "20"
+    val nilai2 = 10
+    println(nilai1.toInt() + nilai2)
+}
