a53a243a6a536e250b319ca829959449b2279f17
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-07-04 21:33:20 +0700

    When as a Expression

--- src/main/kotlin/#028 When as a Expression.kt
+++ src/main/kotlin/#028 When as a Expression.kt
@@ -2,12 +2,12 @@ fun main(){
     val angka = 4
 
     val stringofvalue = when(angka){
-        1 -> print("Nilainya adalah 1")
-        2 -> print("Nilainya adalah 2")
-        3 -> print("Nilainya adalah 3")
-        4 -> print("Nilainya adalah 4")
-        5 -> print("Nilainya adalah 5")
-        else -> print("Nilai Tidak Di Temukan")
+        1 -> "Nilainya adalah 1"
+        2 -> "Nilainya adalah 2"
+        3 -> "Nilainya adalah 3"
+        4 -> "Nilainya adalah 4"
+        5 -> "Nilainya adalah 5"
+        else -> "Nilai Tidak Di Temukan"
     }
 
     println(stringofvalue)
