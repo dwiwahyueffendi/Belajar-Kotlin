31d97a76d87c353c1c2ba09e16a950678922bb35
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-07-07 21:41:02 +0700

    Range ForEach

---
+++ src/main/kotlin/#044 Range ForEach.kt
@@ -0,0 +1,14 @@
+fun main(){
+    print("Masukkan Nilai Awalnya : ")
+    val awal : Int = readLine()!!.toInt()
+    print("Masukkan Batas Nilainya : ")
+    val batas : Int = readLine()!!.toInt()
+    print("Masukkan Nilai Pertambahannya : ")
+    val pertambahannya : Int = readLine()!!.toInt()
+
+    val range = awal.rangeTo(batas) step pertambahannya
+
+    range.forEach{
+        print("$it")
+    }
+}
