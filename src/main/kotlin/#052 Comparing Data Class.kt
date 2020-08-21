8666190b501aaaccac0e882ecab3c5571813edac
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-08-21 17:23:42 +0700

    Comparing Data Class

--- src/main/kotlin/#052 Comparing Data Class.kt
+++ src/main/kotlin/#052 Comparing Data Class.kt
@@ -1,3 +1,34 @@
+class User2(val nama:String, val umur: Int){
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as User2
+
+        if(nama != other.nama) return false
+        if(umur != other.umur) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = nama.hashCode()
+        result = 20 * result + umur
+        return result
+    }
+}
+
+fun main(){
+    val userA = User2("Wahyu", 20)
+    val userB = User2("Wahyu", 20)
+    val userC = User2("Wahyu", 30)
+    val userD = User2("Effendi", 20)
+
+    println(userA.equals(userB))
+    println(userA.equals(userC))
+    println(userA.equals(userD))
+}
+
+/*
 data class dataUser2(val nama: String, val umur: Int)
 
 fun main(){
@@ -9,4 +40,4 @@ fun main(){
     println(userA.equals(userB))
     println(userA.equals(userC))
     println(userA.equals(userD))
-}
+}*/
