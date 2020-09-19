337a767809a237021bc56a41959c6bf21ff8bafd
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-09-19 11:27:10 +0700

    Copying Data Class Object

--- src/main/kotlin/#053 Copying Data Class Objects.kt
+++ src/main/kotlin/#053 Copying Data Class Objects.kt
@@ -3,9 +3,8 @@ data class dataUser3(val nama: String, val umur: Int)
 fun main(){
     val userA1 = dataUser3("Dwi Wahyu", 20)
     val userA2 = userA1.copy()
-    val userA3 = userA1.copy(umur = 21)
 
     println(userA1)
     println(userA2) //Mencopy
-    println(userA3) //Mencopy dan Memodifikasi
+
 }
