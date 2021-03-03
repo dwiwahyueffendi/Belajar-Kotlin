c0282b960bc72ea52a2311ec4e152f7f4b1956f4
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-03-03 23:07:23 +0700

    OOP

---
+++ EduTools/src/Object Oriented/Import dan Packages/#126 Contoh 1.kt
@@ -0,0 +1,12 @@
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sqrt
+import kotlin.math.tan as tangen    //Alias
+//import kotlin.math.*
+
+fun main(){
+    println(PI)
+    println(cos(120.0))
+    println(sqrt(9.0))
+    println(tangen(45.0))
+}
