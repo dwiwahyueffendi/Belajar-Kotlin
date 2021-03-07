9f022597a77d12f454cf2d6d5c844ceda3009d33
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-03-07 23:22:46 +0700

    Generics

---
+++ EduTools/src/Generics/Latihan/#140 Latihan.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val stringResult = getResult("Kotlin")
+    val intResult = getResult(100)
+
+    // Cetak Nilai
+    println("String result: $stringResult")
+    println("Int result: $intResult")
+}
+
+// Menambahkan Fungsi Generic
+fun <T> getResult(args: T): Int {
+    return when (args) {
+        is Int -> args * 5
+        is String -> args.length
+        else -> 0
+    }
+}
